18b85853150c700a9638b7446a10524c8d694b10
MarshallCN <collapselzh@live.com>
2019-08-16 21:27:12 +0100

    change alarm condition

--- ColabAlert.js
+++ ColabAlert.js
@@ -1,17 +1,31 @@
 path='https://marshallcn.github.io/test/src/';
-fav=document.getElementById('favicon-link').href;
-detect = setInterval(function (){
-    if(fav.indexOf('busy')==-1){
-		var sound = document.createElement('audio');
-		sound.id = 'audio-player';
-		sound.src = fav.indexOf('error')>0?path+'beep.ogg':path+'bell.mp3';
-		sound.type = 'audio/mpeg';
-		sound.loop = true;
-		sound.style = 'display:none;';
-		document.body.appendChild(sound);void(0);
-		document.getElementById('audio-player').play();
-    	alert('Process Finished');
-		document.getElementById('audio-player').pause()
-		clearInterval(detect)
-	}
-},1000)
+function setAlarm(a=true){
+	detect = setInterval(function (){
+		fav=document.getElementById('favicon-link').href;
+		if(fav.indexOf('busy')==-1){
+			sound.src = fav.indexOf('error')>0?path+'warn.mp3':path+'movie.mp3';//bell.mp3,beep.ogg
+			sound.play();
+			setTimeout(function(){
+				if(a){alert('Process Finished');}
+				sound.pause();
+				clearInterval(detect);}
+			,100)
+		}
+	},1000)
+}
+if($('#audio-player')==null){
+	sound = document.createElement('audio');
+	sound.id = 'audio-player';
+	sound.type = 'audio/mpeg';
+	// sound.loop = true;
+	sound.src = path+'beep.ogg'
+	sound.style = 'display:none;';
+	document.body.appendChild(sound);
+}
+if($('#alarm-btn')==null){
+	n=document.createElement('colab-toolbar-button');
+	n.id='alarm-btn';
+	n.innerText='Alarm';
+	n.onclick=setAlarm
+	$('#top-toolbar').insertBefore(n,$('colab-connect-button'));
+}setAlarm(a=false);
