de6368ad894f2cbf0b37665a803550ef21c31255
Zehao <16070638+MarshallCN@users.noreply.github.com>
2020-09-15 01:09:21 +0100

    Update config.js

--- noti/config.js
+++ noti/config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyCfUTbrS9FIQUKvqecUXDytzriLIzve5f8'
+  gcmAPIKey: 'AIzaSyAPl1IBVbZA9ZzDY9Ujj3AEXK35cggMx8w'
 };
